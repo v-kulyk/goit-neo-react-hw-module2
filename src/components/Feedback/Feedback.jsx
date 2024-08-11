2e0a767fb300f2e4c6ae5f382bf64aaac17fbbb1
Volodymyr Kulyk <vlad.dev.zp@gmail.com>
2024-08-11 17:02:28 +0300

    refactor

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,11 +1,14 @@
 export default function Feedback({ good, bad, neutral }) {
+  const totalFeedback = bad + good + neutral;
+  const positivePercentage = Math.round((good / totalFeedback) * 100);
+
   return (
     <div>
       <div>Good: {good}</div>
       <div>Neutral: {neutral}</div>
       <div>Bad: {bad}</div>
-      <div>Total: {bad + good + neutral}</div>
-      <div>Positive: {Math.round((good / (bad + good + neutral)) * 100)}%</div>
+      <div>Total: {totalFeedback}</div>
+      <div>Positive: {positivePercentage}%</div>
     </div>
   );
 }
